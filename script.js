1a99995af3c0f591399c42003bc2bcc48394500d
FarhanMoazam <farhanmooazam@gmail.com>
2024-05-13 10:38:29 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+
+///////////// up down nav///////
+
+// $(document).ready(function(){
+//     $('#menu').click(function(){
+//       $('.slide-nav').animate({width: 'toggle', up: '0'}, 'medium');
+//     });
+//   });
+  
+  ////// slide nav////
+
+// $("#menu").click(function(){
+//     $(".slide-nav"  ).toggle("fast");
+// });
+
+
+/////// Right to left nav//
+$(document).ready(function(){
+    $('#menu').click(function(){
+      $('.slide-nav').animate({width: 'toggle', right: '0'}, 'fast');
+    });
+  });
